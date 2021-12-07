887eedf5e3c2fc0365ca2f05c93e8b5f36898808
Satish2021N <satishdevilliers17@gmail.com>
2021-12-07 19:50:28 +0545

    Second Commit

---
+++ src/ProblemNo2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ProblemNo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = sc.next();
+
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+        if (s.equals(rev)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+    }
+}
+
+
